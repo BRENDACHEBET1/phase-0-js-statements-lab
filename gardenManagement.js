7650012b429e5c1d9ccc8a85c5e89fc09fbba4b6
BRENDACHEBET1 <chebetb388@gmail.com>
2026-03-18 01:42:47 +0300

    Update gardenManagement.js to simulate a garden management system

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,27 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+
+//Using if-else statement to check temperature
+if (temperature > 80){
+    console.log("Watering on")
+}else{
+    console.log("Watering off")
+}
+
+
+//Using if-else statement to check time of Day
+if (timeOfDay === "evening" || timeOfDay === "night"){
+    console.log("Lights on")
+}else{
+    console.log("Lights off")
+}
+
+//Using while loop to increment the value of soil moisture
+
+while (soilMoisture < 40){
+     soilMoisture += 5
+     console.log(soilMoisture)
+}
+
